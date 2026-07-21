1b37d6d409228b99341b2b70633d880ea0d76ff7
omarcomweb <omarsalam22@yahoo.com>
2026-07-21 21:58:24 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'pwa-cache-v1784659332226';
-const urlsToCache = [ './', './index.html?v=v1784659332226', './offline.html?v=v1784659332226', './icon-192.png?v=v1784659332226', './icon-512.png?v=v1784659332226', './manifest.json?v=v1784659332226' ];
+const CACHE_NAME = 'pwa-cache-v1784660219011';
+const urlsToCache = [ './', './index.html?v=v1784660219011', './offline.html?v=v1784660219011', './icon-192.png?v=v1784660219011', './icon-512.png?v=v1784660219011', './manifest.json?v=v1784660219011' ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
@@ -29,7 +29,7 @@ self.addEventListener('fetch', event => {
       .then(response => {
         return response || fetch(event.request).catch(() => {
           if (event.request.mode === 'navigate') {
-            return caches.match('./offline.html?v=v1784659332226');
+            return caches.match('./offline.html?v=v1784660219011');
           }
         });
       })
